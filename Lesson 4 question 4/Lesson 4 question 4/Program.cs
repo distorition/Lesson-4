1b5878d0153a9b0a0d7420dd447fa8c6666a5e07
distorition <85959781+distorition@users.noreply.github.com>
2021-06-26 21:43:29 +0300

    Add files via upload

---
+++ Lesson 4 question 4/Lesson 4 question 4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4_question_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = Convert.ToInt32(Console.ReadLine());
+            Fibonachi(n);
+        }
+        static int Fibonachi(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fibonachi(n - 1) + Fibonachi(n - 2);
+            }
+        }
+
+    }
+}
